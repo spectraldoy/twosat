84b1d6585f979f5b054d3249936dabae341b1103
spectraldoy <littleguyisfum@gmail.com>
2024-02-17 15:50:34 -0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::collections::HashMap;
+
+struct TwoCnf {
+    variables: Vec<i32>,
+    formula: 
+}
+
+fn main() {
+    println!("Hello, world!");
+}
+
+fn parse_2cnf(desc: &str) {
+    let mut variables: HashMap<&str, i32> = HashMap::<&str, i32>::new();
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
